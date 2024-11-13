e23a89bb209af17a8851fc76a9a47273449b6a4d
Amal Alqasim <558573@edu.rocmn.nl>
2024-11-13 14:10:39 +0100

    ..

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a78f782d7e012a1eb58c976292ded63d6c74563a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a78f782d7e012a1eb58c976292ded63d6c74563a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'bluerhinos/phpmqtt' => array(
+            'pretty_version' => '1.0.1',
+            'version' => '1.0.1.0',
+            'reference' => 'fe4b6b2fe3d1b651fe1456e147ad4f044fa70603',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../bluerhinos/phpmqtt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
